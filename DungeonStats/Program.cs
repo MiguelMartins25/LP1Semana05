486066877b6b211d4b6e81ab384e853b6f89d760
Miguel Martins <miguelarbmartins@gmail.com>
2026-03-19 16:17:07 +0000

    ramo core-logic e criação dos métodos

--- DungeonStats/Program.cs
+++ DungeonStats/Program.cs
@@ -6,7 +6,26 @@ namespace DungeonStats
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            
+        }
+
+        static int Damage(int attack, int defense)
+        {
+            int result = attack - defense;
+            return result < 0 ? 0 : result; //minimo 0
+        }
+
+        static int Damage(int attack)
+        {
+            return attack;
+        }
+
+        static int CriticalHit(int damage)
+        {
+            if (damage <= 0)
+                return 0;
+
+            return damage + CriticalHit(damage - 1);
         }
     }
 }
